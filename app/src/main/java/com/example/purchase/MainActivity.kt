e2b8702d6bd28731fc3129511b9dd3d0ef9b0f83
Roman Myasoedov <375298131149@yandex.by>
2020-05-16 19:48:17 +0300

    Готовый дизайн тулбара

--- app/src/main/java/com/example/purchase/MainActivity.kt
+++ app/src/main/java/com/example/purchase/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.purchase
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +11,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        requestedOrientation = (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setSupportActionBar(toolbar) //Установить тулбар
+        requestedOrientation = (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) //Запрет на горизонтальный разворот
 
+    }
+
+
+    //Добавить контекстное меню на тулбар верхней части экрана
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 }
