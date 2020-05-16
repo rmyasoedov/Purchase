c5cae75586a51cba08e1566493f95c4ccaf568dc
Roman Myasoedov <375298131149@yandex.by>
2020-05-16 18:43:14 +0300

    First Commit

--- app/src/main/java/com/example/purchase/MainActivity.kt
+++ app/src/main/java/com/example/purchase/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.purchase
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        requestedOrientation = (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
     }
 }
